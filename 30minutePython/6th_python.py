71cc20fc62216ec4abd7c17fec62eb141ca34fe1
panghoon <leegwanghoon95@gmail.com>
2022-08-09 00:06:08 +0900

    30분 파이썬 영어문서공부 : #6

--- 30minutePython/6th_python.py
+++ 30minutePython/6th_python.py
@@ -14,4 +14,23 @@ class Six_th:
 
     list1.append('addData') # 데이터 추가하기
 
+    letters = ['a', 'b', 'c', 'd']
+    letters[1:3]
+    letters[1:3] = ['B', 'C']
+    letters[1:3] = ['B', 'C', 0]
+    letters[1:2] = []
+    letters[:] = []
+
+
+    a = [1,2,3]
+    b = [4,5,6]
+    c = [a, b] # 2차원 배열
+
+    a, b = 0, 1 # 한꺼번에 여러개 변수 선언 가능
+
+    while b<10:
+        print(b)
+        a, b = b, a  # -> multiple assignment 가 가능하다.
+        # 다른 언어들은 temporary  변수를 사용하지만 파이썬은 그렇지 않다.
+
 
