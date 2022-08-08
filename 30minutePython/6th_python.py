9ad302bb1e6614743dd5c0bc5bdbd4376b3fa09b
panghoon <leegwanghoon95@gmail.com>
2022-08-08 23:53:38 +0900

    30분 파이썬 영어문서공부 : #6

---
+++ 30minutePython/6th_python.py
@@ -0,0 +1,17 @@
+#List
+
+
+
+class Six_th:
+    list1 = [1, 4, 5, 6, 7, '문자열']
+    print(list1[-3:]) #[6, 7, '문자열']
+
+    list[:] # 레퍼런스를 참조하는 복사본
+
+    list1 + [23,54,12, '붙이기']
+
+    list1[3] = 'fixDATA'
+
+    list1.append('addData') # 데이터 추가하기
+
+
