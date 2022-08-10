ebc290b9ec39cf18f50714b34aeafad0a004a5cb
panghoon <leegwanghoon95@gmail.com>
2022-08-10 23:41:26 +0900

    30분 파이썬 영어문서공부 : #7

---
+++ 30minutePython/7th_python.py
@@ -0,0 +1,15 @@
+#
+
+class Seven_th:
+    if 0:
+        print(123)
+    # 파이썬에서 0은 false를 의미힌다.
+
+    if -1:
+        print(123)
+    #파이썬에서 Non-zero는 true를 의미한다.
+
+    while b<100:
+        print(b, end=',') # end argument를 사용해서 자동 줄 바꿈을 막는다.
+
+
